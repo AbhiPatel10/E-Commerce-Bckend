73ad0df03432346216c075205d2532b0d99374cf
AbhiPatelBizLine <abhipatel.bizline@linics.co>
2026-02-24 17:45:16 +0530

    feat: Add global 404 fallback for unavailable endpoints and correct production build and deployment paths.

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import serverlessExpress from '@vendia/serverless-express';
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 let cachedServer: any;
 
@@ -30,6 +30,14 @@ async function bootstrap() {
   );
 
   await app.init();
+
+  // ✅ Global 404 fallback (AFTER Nest routes)
+  expressApp.use((req: Request, res: Response) => {
+    res.status(404).json({
+      message: 'This endpoint is not available in E-Commerce.',
+      path: req.originalUrl,
+    });
+  });
 
   return serverlessExpress({ app: expressApp });
 }
